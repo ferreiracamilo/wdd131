3ecc6d1e107cc647e0234eaf2dcf5012ecdee857
ferreiracamilo_parqa <camilo.ferreira@partech.com>
2024-05-02 23:06:10 -0300

    update script to work with screen width as well

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,6 +2,12 @@ const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+    // Check screen width to remove the class open that works for the collapsible menu
+    if (window.innerWidth >= 900) {
+        navigation.classList.remove('open');
+        hamButton.classList.remove('open');
+    } else {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    }
 });
