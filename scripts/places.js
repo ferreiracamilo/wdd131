6965f621fab6a1a3ab7a55eb07150249f003cec7
ferreiracamilo_parqa <camilo.ferreira@partech.com>
2024-05-11 00:52:55 -0300

    update script comments

--- scripts/places.js
+++ scripts/places.js
@@ -3,16 +3,15 @@ function calculateWindChill() {
     var temperature = parseFloat(document.getElementById("tempData").textContent);
     var windSpeed = parseFloat(document.getElementById("windSpeedData").textContent);
 
-    // Check if temperature is 10°C or less
+    // Check if temperature is 10°C or less based on table provided on assignment page
     if (temperature <= 10) {
-        // Calculate wind chill
+        // Calculate wind chill because temperature is meant to be considered for this calculation
         var windChill = 13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature * Math.pow(windSpeed, 0.16));
-        return windChill.toFixed(2) + " °C"; // Return wind chill rounded to 2 decimal places with unit
+        return windChill.toFixed(2) + " °C"; // Return wind chill rounded to 2 decimal places with unit along celsius symbol
     } else {
         return "N/A"; // Return N/A if temperature is greater than 10°C
     }
 }
 
-// Call the function and update the content of the span with id windChillData
 var windChillResult = calculateWindChill();
 document.getElementById("windChillData").textContent = windChillResult;
