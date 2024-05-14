c884669c87ffb4e7d285401e04631368f7320ff1
ferreiracamilo_parqa <camilo.ferreira@partech.com>
2024-05-13 23:44:18 -0300

    Added to conditional the revision of the windspeed in order to decide if calculate or not the windchill

--- scripts/places.js
+++ scripts/places.js
@@ -4,7 +4,8 @@ function calculateWindChill() {
     var windSpeed = parseFloat(document.getElementById("windSpeedData").textContent);
 
     // Check if temperature is 10°C or less based on table provided on assignment page
-    if (temperature <= 10) {
+    //Added to conditional the revision of windSpeedData
+    if (temperature <= 10 && windSpeed > 4.8) {
         // Calculate wind chill because temperature is meant to be considered for this calculation
         var windChill = 13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature * Math.pow(windSpeed, 0.16));
         return windChill.toFixed(2) + " °C"; // Return wind chill rounded to 2 decimal places with unit along celsius symbol
