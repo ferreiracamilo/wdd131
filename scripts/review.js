7d6f4ae172e623bb433c4c244c9d840bfcfd247c
ferreiracamilo_parqa <camilo.ferreira@partech.com>
2024-05-25 23:29:30 -0300

    Change visits to reviews and set text of reviews span

--- scripts/review.js
+++ scripts/review.js
@@ -2,7 +2,9 @@
  * Load local storage
  * **/ 
 // Initialize display element variable in review page
-const visitsDisplay = document.querySelector(".visits");
+const reviewsDisplay = document.querySelector(".reviews");
 
-// Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+// Get the stored VALUE for the numreviews-ls KEY in localStorage if it exists. If the numreviews KEY is missing, then assign 0 to the numreviews variable.
+let numreviews = Number(window.localStorage.getItem("numreviews-ls")) || 0;
+
+reviewsDisplay.textContent = numreviews;
