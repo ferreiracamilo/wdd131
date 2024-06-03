925931f307a9a1e112ff2508c84ba56d0dff610a
ferreiracamilo_parqa <camilo.ferreira@partech.com>
2024-06-02 21:09:54 -0300

    update name

--- project/scripts/countVisits.js
+++ project/scripts/countVisits.js
@@ -2,18 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // Verify if current page visited by user is index page
     if (window.location.pathname.endsWith('index.html') || window.location.pathname === '/') {
         // Verify variable exists
-        if (localStorage.getItem('unitepawvisit')) {
+        if (localStorage.getItem('unitedpawsvisit')) {
             // Increase by one unit
-            let visitCount = parseInt(localStorage.getItem('unitepawvisit'));
+            let visitCount = parseInt(localStorage.getItem('unitedpawsvisit'));
             visitCount += 1;
-            localStorage.setItem('unitepawvisit', visitCount);
+            localStorage.setItem('unitedpawsvisit', visitCount);
         } else {
             // Define the new variable into 1
-            localStorage.setItem('unitepawvisit', 1);
+            localStorage.setItem('unitedpawsvisit', 1);
         }
 
         // Get current value of unitedpawvisit
-        let currentVisitCount = localStorage.getItem('unitepawvisit');
+        let currentVisitCount = localStorage.getItem('unitedpawsvisit');
 
         // Print this value into footer
         let visitQtyElement = document.getElementById('visitQty');
